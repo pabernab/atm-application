9ad932205367501c764cd3bb433a011cafed328b
Luke Brouwer <53581714+lukebrouwer2000@users.noreply.github.com>
2020-10-29 19:22:41 -0700

    another round of big changes
    added input validation and verification for user input. ensures all forms are filled and not null, passwords exist and match.
    
    Paths
    Registration form is filled and information puts new account into database
    Passwords do not match, user is given an error
    Registration form is filled but credentials already exist, user is given an error

--- process.php
+++ process.php
@@ -16,36 +16,98 @@
     }
 
     // test display of host connection info
-    echo $mysqli->host_info . "\n";
-    
-
-    // data points we will be inserting into the database
-    // pulled from the registration form
-    $userName = $_POST["userName"];
-    $userPassword = $_POST["userPassword"];
-    $userEmailAddress = $_POST["userEmailAddress"];
-    $userFirstName = $_POST["userFirstName"];
-    $userLastName = $_POST["userLastName"];
+    // echo $mysqli->host_info . "\n";
 
     
-    // simple sql syntax to insert into our registration
-    // database, formatted for visual clarity
-    $sql = "INSERT into userRegistration (
-        userName,
-        userPassword,
-        userEmailAddress,
-        userFirstName,
-        userLastName
-    ) 	VALUES (
-        '$userName',
-        '$userPassword',
-        '$userEmailAddress',
-        '$userFirstName',
-        '$userLastName'
-    )";
+    // all forms are properly filled and no null submissions
+    if (isset($_POST["userName"]) 
+        && isset($_POST["userPassword"]) 
+        && isset($_POST["userEmailAddress"])
+        && isset($_POST["userFirstName"]) 
+        && isset($_POST["userLastName"]) 
+        && isset($_POST["repassword"])){
 
-    // querying our connected database with the given data points
-    // inserting form information
-    $results = mysqli_query($mysqli, $sql);
+            // simple test pathing
+            echo "<br>";
+            echo "All forms are set";
+            echo "<br>";
 
+
+            // making sure both passwords are equal
+            if ($_POST["userPassword"] == $_POST["repassword"]){
+
+                // simple test pathing
+                echo "<br>";
+                echo "Passwords are equal";
+                echo "<br>";
+    
+                // data points we will be inserting into the database
+                // pulled from the registration form
+                $userName = $_POST["userName"];
+                $userPassword = $_POST["userPassword"];
+                $userEmailAddress = $_POST["userEmailAddress"];
+                $userFirstName = $_POST["userFirstName"];
+                $userLastName = $_POST["userLastName"];
+    
+    
+                // simple sql syntax to insert into our registration
+                // database, formatted for visual clarity
+                $sql = "INSERT into userRegistration (
+                    userName,
+                    userPassword,
+                    userEmailAddress,
+                    userFirstName,
+                    userLastName
+                ) 	VALUES (
+                    '$userName',
+                    '$userPassword',
+                    '$userEmailAddress',
+                    '$userFirstName',
+                    '$userLastName'
+                )";
+            
+                // querying our connected database with the given data points
+                // inserting form information
+                $results = mysqli_query($mysqli, $sql);
+    
+                // making sure we were able to insert the information properly into
+                // our MySQL database
+                if ($results){
+                    echo "<br>";
+                    echo "\n\nInformation inserted into database.\n\n";
+                    echo "<br>";
+                    
+                }
+
+                // in the event we somehow were unable to insert the information
+                // thus not being able to create the account
+                else {
+                    echo "<br>";
+                    echo "Error: Information not inserted into database.";
+                    echo "<br>";
+                    echo "Those login credentials already exist in our database.";
+                    echo "<br>";
+                }
+            }
+
+            // if passwords don't match
+            else {
+
+                echo "<br>";
+                echo "Both passwords must match.";
+                echo "<br>";
+            }
+
+    }
+    else {
+
+        echo "<br>";
+        echo "All forms must be filled.";
+        echo "<br>";
+    }
+        // close connection
+        mysqli_close($mysqli);
+    
+
+   
 ?>
