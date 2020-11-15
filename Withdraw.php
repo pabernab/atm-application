06f8a4522f9f47eb56ccdf16a0079b479f326da9
alenba2 <baekallen@gmail.com>
2020-11-15 12:18:17 -0800

    Added in Deposit and Withdraw, Connection to SQL

--- Withdraw.php
+++ Withdraw.php
@@ -1,5 +1,33 @@
 <?php
 
+// CONNECTING SERVER
+$serverEndpoint = 'mysqldb.cjezeavsieu7.us-west-1.rds.amazonaws.com';
+$serverUserName = 'butteadmin';
+$serverPassword = 'buttecmpe131';
+$dbname = 'registration';
+
+// creating a new server connection using our preset AWS login values
+$mysqli = new mysqli($serverEndpoint, $serverUserName, $serverPassword, $dbname, 3306);
+
+// simple error catch if we are unable to connect to the MySQL Database
+if ($mysqli->connect_errno) {
+  echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+//END CONNECTION
+  mysqli_close($mysqli);
+
 
 ?>
 
