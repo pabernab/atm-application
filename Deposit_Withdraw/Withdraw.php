83ab4c734b47b976d914c381a1fa5e6214b3fe00
alenba2 <baekallen@gmail.com>
2020-11-25 17:38:11 -0800

    check

--- Deposit_Withdraw/Withdraw.php
+++ Deposit_Withdraw/Withdraw.php
@@ -123,7 +123,7 @@ if(isset($_POST["amount"]) && isset($_POST["AccountNumber"]))
       </p>
 
       <p class = "regularFont">
-        Put in value between 0.00 to 4000.00
+        Put in value between 0.01 to 4000.00
       </p>
 
       <p class = "regularFont">
@@ -201,7 +201,7 @@ if(isset($_POST["amount"]) && isset($_POST["AccountNumber"]))
       <br>
       <br>
 
-      <input type = "number" name = "amount" min = "0.00" max = "4000.00" step = "0.01">
+      <input type = "number" name = "amount" min = "0.01" max = "4000.00" step = "0.01">
       <input type = "submit" value = "submit">
     </form>
 
